c8b782e21c7881fc60cf872ab08feeab893f25a0
FML3426 <fml3426@gmail.com>
2018-01-04 21:47:14 +0800

    ClassLoaderLocal功能补全

--- xo-mapper-core/src/main/java/me/ml3426/xomapper/ContextClassLoaderLocal.java
+++ xo-mapper-core/src/main/java/me/ml3426/xomapper/ContextClassLoaderLocal.java
@@ -5,6 +5,7 @@ import java.security.PrivilegedAction;
 import java.util.Map;
 import java.util.WeakHashMap;
 
+import java.util.function.Supplier;
 import me.ml3426.xomapper.util.LogUtil;
 
 /**
@@ -16,10 +17,73 @@ import me.ml3426.xomapper.util.LogUtil;
  * @param <T> 此Local对应储存的实例的类型
  * @author ml3426
  * @since 0.0.1
+ * @see Thread#getContextClassLoader()
  */
 public class ContextClassLoaderLocal<T> {
     
     private final Map<ClassLoader, T> valueCache = new WeakHashMap<>(16);
+
+    private T globalValue;
+
+    private boolean globalValueInited = false;
+
+    /**
+     * 创建一个空的ContextClassLoaderLocal
+     * @see #withInitial(Supplier)
+     */
+    public ContextClassLoaderLocal() {
+    }
+
+    public static <S> ContextClassLoaderLocal<S> withInitial(final Supplier<S> initSupplier) {
+        return new SuppliedContextClassLoaderLocal<>(initSupplier);
+    }
+
+    /**
+     *
+     *
+     * @return
+     */
+    protected T initialValue() {
+        return null;
+    }
+
+    /**
+     *
+     *
+     * @return
+     */
+    public synchronized T get() {
+        final ClassLoader ccl = getContextClassLoader();
+
+        if (ccl != null) {
+            return valueCache.computeIfAbsent(ccl, loader -> initialValue());
+        } else {
+            if (!globalValueInited) {
+                globalValueInited = true;
+                return globalValue = initialValue();
+            }
+
+            return globalValue;
+        }
+    }
+
+    /**
+     *
+     *
+     * @param value
+     */
+    public synchronized void set(T value) {
+        final ClassLoader ccl = getContextClassLoader();
+
+        if (ccl != null) {
+            valueCache.put(ccl, value);
+        }
+
+        if (!globalValueInited) {
+            globalValueInited = true;
+            globalValue = value;
+        }
+    }
     
     private static ClassLoader getContextClassLoader() {
         return AccessController.doPrivileged((PrivilegedAction<ClassLoader>) () -> {
@@ -30,5 +94,24 @@ public class ContextClassLoaderLocal<T> {
                 return null;
             }
         });
+    }
+
+    /**
+     * 通过指定的Supplier保存线程上下文本地对象的保存器，通过制定的
+     *
+     * @param <T>
+     */
+    private final static class SuppliedContextClassLoaderLocal<T> extends ContextClassLoaderLocal<T> {
+
+        private final Supplier<T> valueSupplier;
+
+        public SuppliedContextClassLoaderLocal(Supplier<T> valueSupplier) {
+            this.valueSupplier = valueSupplier;
+        }
+
+        @Override
+        protected T initialValue() {
+            return valueSupplier.get();
+        }
     }
 }
