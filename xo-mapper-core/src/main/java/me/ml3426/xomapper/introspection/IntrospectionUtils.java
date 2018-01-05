23b8b6b4a91fc81ad9e2bc8b58e6ca1bd0a35f3e
Steven Zhang <fml3426@gmail.com>
2018-01-05 18:00:37 +0800

    Java自省相关工具类补全

--- xo-mapper-core/src/main/java/me/ml3426/xomapper/introspection/IntrospectionUtils.java
+++ xo-mapper-core/src/main/java/me/ml3426/xomapper/introspection/IntrospectionUtils.java
@@ -1,20 +1,20 @@
 package me.ml3426.xomapper.introspection;
 
-import org.apache.commons.lang3.tuple.Pair;
-
+import com.sun.xml.internal.ws.util.StringUtils;
 import java.beans.BeanInfo;
 import java.beans.IntrospectionException;
 import java.beans.Introspector;
+import java.beans.MethodDescriptor;
 import java.beans.PropertyDescriptor;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.WeakHashMap;
-
 import me.ml3426.xomapper.ContextClassLoaderLocal;
 import me.ml3426.xomapper.exception.IntropectionRuntimeException;
 import me.ml3426.xomapper.util.LogUtil;
+import org.apache.commons.lang3.tuple.MutablePair;
 
 /**
  * Java自省相关工具类
@@ -25,9 +25,11 @@ import me.ml3426.xomapper.util.LogUtil;
 public class IntrospectionUtils {
     
     private final Map<Class, BeanInfo> beanInfoCache = new WeakHashMap<>(16);
+
+    private final static String SETTER_PREFIX = "set";
     
-    private final Map<Class, Pair<List<PropertyDescriptor>, List<PropertyDescriptor>>> propertyCache
-            = new WeakHashMap<>(16);
+    private final Map<Class, MutablePair<List<PropertyDescriptor>, List<PropertyDescriptor>>>
+            propertyCache = new WeakHashMap<>(16);
 
     private final static ContextClassLoaderLocal<IntrospectionUtils>
             UTILS_CLASSLOADER_LOCAL = ContextClassLoaderLocal.withInitial(IntrospectionUtils::new);
@@ -38,47 +40,85 @@ public class IntrospectionUtils {
     private static IntrospectionUtils getInstance() {
         return UTILS_CLASSLOADER_LOCAL.get();
     }
-    
-    private static List<PropertyDescriptor> getBeanGetters(final Class beanType) {
-        
-        final BeanInfo beanInfo = getInstance().beanInfoCache.computeIfAbsent(beanType, key -> {
-            try {
-                return Introspector.getBeanInfo(key, Object.class);
-            } catch (IntrospectionException ex) {
-                throw new IntropectionRuntimeException(ex);
-            }
-        });
 
-        final PropertyDescriptor[] propertyDescriptors = beanInfo.getPropertyDescriptors();
-        final List<PropertyDescriptor> getters = new ArrayList<>(propertyDescriptors.length);
-        for (PropertyDescriptor descriptor : propertyDescriptors) {
-            if (descriptor.getReadMethod() != null) {
-                getters.add(descriptor);
+    public static List<PropertyDescriptor> getBeanGetters(final Class beanType) {
+
+        final MutablePair<List<PropertyDescriptor>, List<PropertyDescriptor>> properties =
+                getInstance().propertyCache.computeIfAbsent(beanType, key -> MutablePair.of(null, null));
+
+        final List<PropertyDescriptor> cacheGetters = properties.getLeft();
+        if (cacheGetters == null) {
+            final BeanInfo beanInfo = getInstance().beanInfoCache.computeIfAbsent(beanType, key -> {
+                try {
+                    return Introspector.getBeanInfo(key, Object.class);
+                } catch (IntrospectionException ex) {
+                    throw new IntropectionRuntimeException(ex);
+                }
+            });
+
+            final PropertyDescriptor[] propertyDescriptors = beanInfo.getPropertyDescriptors();
+            final List<PropertyDescriptor> getters = new ArrayList<>(propertyDescriptors.length);
+            for (PropertyDescriptor descriptor : propertyDescriptors) {
+                if (descriptor.getReadMethod() != null) {
+                    getters.add(descriptor);
+                }
             }
+            properties.setLeft(getters);
+            return getters;
         }
-        
-        return getters;
+
+        return cacheGetters;
     }
     
-    private static List<PropertyDescriptor> getBeanSetters(final Class beanType) {
-        final BeanInfo beanInfo = getInstance().beanInfoCache.computeIfAbsent(beanType, key -> {
-            try {
-                return Introspector.getBeanInfo(key, Object.class);
-            } catch (IntrospectionException ex) {
-                throw new IntropectionRuntimeException(ex);
-            }
-        });
+    public static List<PropertyDescriptor> getBeanSetters(final Class beanType) {
 
-        final PropertyDescriptor[] propertyDescriptors = beanInfo.getPropertyDescriptors();
-        final List<PropertyDescriptor> setters = new ArrayList<>(propertyDescriptors.length);
-        for (PropertyDescriptor descriptor : propertyDescriptors) {
-            if (descriptor.getWriteMethod() != null) {
-                setters.add(descriptor);
-            } else {
-                
+        final MutablePair<List<PropertyDescriptor>, List<PropertyDescriptor>> properties =
+                getInstance().propertyCache.computeIfAbsent(beanType, key -> MutablePair.of(null, null));
+
+        final List<PropertyDescriptor> cacheSetters = properties.getRight();
+        if (cacheSetters == null) {
+            final BeanInfo beanInfo = getInstance().beanInfoCache.computeIfAbsent(beanType, key -> {
+                try {
+                    return Introspector.getBeanInfo(key, Object.class);
+                } catch (IntrospectionException ex) {
+                    throw new IntropectionRuntimeException(ex);
+                }
+            });
+
+            final PropertyDescriptor[] propertyDescriptors = beanInfo.getPropertyDescriptors();
+            final List<PropertyDescriptor> setters = new ArrayList<>(propertyDescriptors.length);
+            for (PropertyDescriptor descriptor : propertyDescriptors) {
+                if (descriptor.getWriteMethod() != null) {
+                    setters.add(descriptor);
+                } else {
+                    try {
+                        descriptor.setWriteMethod(findBuilderStyleSetter(beanInfo, descriptor));
+                    } catch (IntrospectionException ex) {
+                        LogUtil.error(() -> "Builder模式setter查询失败", ex);
+                    }
+                }
+            }
+            properties.setRight(setters);
+            return setters;
+        }
+
+        return cacheSetters;
+    }
+
+    private static Method findBuilderStyleSetter(final BeanInfo beanInfo, final PropertyDescriptor descriptor) {
+        final MethodDescriptor[] methodDescriptors = beanInfo.getMethodDescriptors();
+
+        final String methodName = SETTER_PREFIX + StringUtils.capitalize(descriptor.getName());
+        for (MethodDescriptor methodDescriptor : methodDescriptors) {
+            final Method currentMethod = methodDescriptor.getMethod();
+
+            if (methodDescriptor.getName().equals(methodName)
+                    && currentMethod.getReturnType().equals(beanInfo.getBeanDescriptor().getBeanClass())
+                    && currentMethod.getParameterCount() == 1
+                    && currentMethod.getParameterTypes()[0].equals(descriptor.getPropertyType())) {
+                return currentMethod;
             }
         }
+        return null;
     }
-    
-    private static Method findBuilderStyleSetter(final BeanInfo beanInfo, final PropertyDescriptor) 
 }
